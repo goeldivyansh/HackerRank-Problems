49770a1ec716d66a8124f205f9dff63687a12e66
Divyansh goel <divyanshgoel.19@gmail.com>
2020-08-16 01:00:22 +0530

    adding 4 programs

---
+++ ProblemSolving/Gemstones.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the gemstones function below.
+    static int gemstones(String[] arr) {
+        int n = arr.length;
+        int count=0,j,isPresent=1;
+
+        for(char i='a';i<='z';i++)
+        {
+            isPresent=1;
+            for(j=0;j<n;j++)
+            {
+                isPresent = arr[j].indexOf(i);
+                if(isPresent == -1)
+                {
+                   break;
+                }
+            }
+            if(isPresent != -1) count++; 
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String arrItem = scanner.nextLine();
+            arr[i] = arrItem;
+        }
+
+        int result = gemstones(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
